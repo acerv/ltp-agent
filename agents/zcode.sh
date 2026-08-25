bbd9d60a520859fcd46d7e81d3b527247cec4f97
Andrea Cervesato <andrea.cervesato@suse.com>
2026-08-25 10:28:33 +0200

    agents: add zcode agent setup
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

---
+++ agents/zcode.sh
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2026 Andrea Cervesato <andrea.cervesato@suse.com>
+#
+# Per-agent install paths for ZCode.
+# Sourced by setup.sh.
+
+export SKILL_BASE_DIR="$HOME/.zcode/skills"
+export COMMANDS_DIR="$HOME/.zcode/commands"
+export SKILL_FILE_NAME="SKILL.md"
